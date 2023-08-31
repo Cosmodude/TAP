bf5fc1ca7e418e0126c6c2c5915c9a4081d1423d
Cosmodude <vladislav.lenskii@gmail.com>
2023-08-31 15:40:32 +0900

    decresed the price

--- scripts/mintNFT.ts
+++ scripts/mintNFT.ts
@@ -13,7 +13,7 @@ export async function run(provider: NetworkProvider, args: string[]) {
     const nftCollection = provider.open(NftCollection.createFromAddress(address));
 
     const mint = await nftCollection.sendMintNft(provider.sender(),{
-        value: toNano("1"),  // 0.015 for gas ~ 0.02
+        value: toNano("0.02"),  // 0.015 for gas ~ 0.02
         amount: toNano("0.014"),  // for gas + 0.01 of storage (usually 0.05)
         itemIndex: 0,
         itemOwnerAddress: myAddress,
