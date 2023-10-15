7728f2107d7f66f685a59f350650fe952d36d72a
Cosmodude <vladislav.lenskii@gmail.com>
2023-10-15 15:58:41 +0900

    deploy and mint scripts created

--- scripts/deploySbtCollection.ts
+++ scripts/deploySbtCollection.ts
@@ -1,9 +1,21 @@
-import { toNano } from 'ton-core';
+import { Address, beginCell, toNano } from 'ton-core';
 import { SbtCollection } from '../wrappers/SbtCollection';
 import { compile, NetworkProvider } from '@ton-community/blueprint';
+import { buildCollectionContentCell} from '../wrappers/contentHelpers/offchain';
+
+let myAddress: Address = Address.parse("kQAXUIBw-EDVtnCxd65Z2M21KTDr07RoBL6BYf-TBCd6dTBu");
 
 export async function run(provider: NetworkProvider) {
-    const sbtCollection = provider.open(SbtCollection.createFromConfig({}, await compile('SbtCollection')));
+    const sbtCollection = provider.open(SbtCollection.createFromConfig({
+    ownerAddress: myAddress,
+    nextItemIndex: 0,
+    collectionContent: buildCollectionContentCell({
+        collectionContent: 'https://raw.githubusercontent.com/TonAttendanceProtocol/Smart_Contracts/main/sampleCollectionMetadata.json',  // collection metadata
+        commonContent: ' '     // for sbt items 
+    }),
+    sbtItemCode: await compile("SbtItem"),
+
+    },await compile('SbtCollection')));
 
     await sbtCollection.sendDeploy(provider.sender(), toNano('0.05'));
 
