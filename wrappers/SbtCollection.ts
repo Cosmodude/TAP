62177539caa52eaf02f02a1d9221bcb0f3eda282
Cosmodude <vladislav.lenskii@gmail.com>
2023-10-15 15:57:56 +0900

    updated wrapper

--- wrappers/SbtCollection.ts
+++ wrappers/SbtCollection.ts
@@ -1,9 +1,29 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
+import { 
+    Address, 
+    beginCell, 
+    Cell, 
+    Contract, 
+    contractAddress, 
+    ContractProvider, 
+    Sender, 
+    SendMode, 
+} from 'ton-core';
+import { buildCollectionContentCell, decodeOffChainContent, encodeOffChainContent } from './contentHelpers/offchain';
 
-export type SbtCollectionConfig = {};
+export type SbtCollectionConfig = {
+    ownerAddress: Address;
+    nextItemIndex: number;
+    collectionContent: Cell;
+    sbtItemCode: Cell;
+};
 
 export function sbtCollectionConfigToCell(config: SbtCollectionConfig): Cell {
-    return beginCell().endCell();
+    return beginCell()
+        .storeAddress(config.ownerAddress)
+        .storeUint(config.nextItemIndex, 64)
+        .storeRef(config.collectionContent)
+        .storeRef(config.sbtItemCode)
+        .endCell();
 }
 
 export class SbtCollection implements Contract {
@@ -26,4 +46,32 @@ export class SbtCollection implements Contract {
             body: beginCell().endCell(),
         });
     }
+
+    async sendMintSbt(provider: ContractProvider, via: Sender,
+        opts: {
+            value: bigint;
+            queryId: number;
+            itemIndex: number;
+            itemOwnerAddress: Address;
+            itemContent: string;
+            amount: bigint;
+        }
+        ) {
+            const sbtContent = encodeOffChainContent(opts.itemContent);
+            
+            const sbtMessage = beginCell();
+            sbtMessage.storeAddress(opts.itemOwnerAddress)
+            sbtMessage.storeRef(sbtContent)
+            await provider.internal(via, {
+                value: opts.value,
+                sendMode: SendMode.PAY_GAS_SEPARATELY,
+                body: beginCell()
+                    .storeUint(1,32)  // operation
+                    .storeUint(opts.queryId,64)
+                    .storeUint(opts.itemIndex,64)
+                    .storeCoins(opts.amount)
+                    .storeRef(sbtMessage)
+                .endCell()
+            })
+        }
 }
